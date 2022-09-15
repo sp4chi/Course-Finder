cc993a7f6b0708fcf756f10ed251b98f53d7a734
kaushik.bora <herock76@gmail.com>
2022-09-16 03:30:45 +0530

    2nd phase complete

--- src/Components/MyPagination/MyPagination.jsx
+++
@@ -1,9 +0,0 @@
-import { Pagination, Box } from "@mui/material";
-import { Container } from "@mui/system";
-
-const MyPagination = () => {
-    return <Container component={Box} py={3} m={10}>
-        <Pagination count={10} variant="outlined" />
-    </Container>
-}
-export default MyPagination
